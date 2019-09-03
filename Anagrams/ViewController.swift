ca4012e3947fbe5bc92d2379028ec77ac99c26c6
Mihail87 <leontemihai@gmail.com>
2019-09-03 15:56:55 +0300

    Initial project setup

---
+++ Anagrams/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anagrams
+//
+//  Created by Mihai Leonte on 9/3/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
