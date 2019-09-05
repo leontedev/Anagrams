53d478409ae9ec623468b81a14085c13aaf784a3
Mihail87 <leontemihai@gmail.com>
2019-09-05 11:02:40 +0300

    Third challenge - Add a left bar button item that calls startGame(), so users can restart with a new word whenever they want to.

--- Anagrams/ViewController.swift
+++ Anagrams/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UITableViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -32,7 +34,7 @@ class ViewController: UITableViewController {
         
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -98,7 +100,6 @@ class ViewController: UITableViewController {
         } else {
             showErrorMessage(title: "Word too short", message: "The word needs to be at least three characters long.")
         }
-        
         
         
     }
